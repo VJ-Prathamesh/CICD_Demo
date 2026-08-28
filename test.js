c6fb41664d954bbc5f555bc10152d2e4df7696c7
Prathamesh Jadhav <prathmesh.jadhav@cctech.co.in>
2026-08-28 14:13:42 +0530

    added testcases

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 
-console.log('Running test: 2 + 2 = 5 (Intentional failure for demo)');
+console.log('Running test: 2 + 2 = 4 (Intentional failure for demo)');
 
 try {
   // To make the pipeline pass, change the 5 to a 4 below
-  assert.strictEqual(2 + 2, 5, "Intentional failure: 2 + 2 does not equal 5!");
+  assert.strictEqual(2 + 2, 4, "Intentional failure: 2 + 2 does not equal 4!");
   console.log('Test passed!');
 } catch (error) {
   console.error('❌ Test Failed:', error.message);
